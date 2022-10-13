e827c613d2229f7d2c65c8c2bf2378e5563dc7a7
igorjunio2015 <igorjunio2015@gmail.com>
2022-10-13 17:18:14 -0300

    hotfix: remove field 'cargo: 3358f544d27546aebcfeed9a4d4ada87fccbbe29'

--- src/routes/pipedrive.route.js
+++ src/routes/pipedrive.route.js
@@ -8,8 +8,12 @@ const pipedriveController = require("../controllers/pipedrive.controller");
  */
 
 routes.post("/v1/addDeal", validarBody, async (req, res) => {
+<<<<<<< HEAD
     var { titulo, pipelineId } = req.body;
     const retornoPipedrive = await pipedriveController.criarNegocioPipedrive({ titulo, pipelineId });
+=======
+    const retornoPipedrive = await pipedriveController.criarNegocioPipedrive(req.body);
+>>>>>>> debfc91 (hotfix: remove field 'cargo: 3358f544d27546aebcfeed9a4d4ada87fccbbe29')
     if (retornoPipedrive.erro) return res.status(500).json(retornoPipedrive);
     return res.status(201).json(retornoPipedrive);
 })
@@ -21,6 +25,18 @@ routes.post("/v1/addDeal", validarBody, async (req, res) => {
 function validarBody(req, res, next) {
     if (!req.body.titulo) return res.status(404).json({ erro: true, message: "'titulo' not found in body" });
     if (!req.body.pipelineId) return res.status(404).json({ erro: true, message: "'pipelineId' not found in body" });
+<<<<<<< HEAD
+=======
+    if (!req.body.nome) return res.status(404).json({ erro: true, message: "'nome' not found in body" });
+    if (!req.body.sobrenome) return res.status(404).json({ erro: true, message: "'sobrenome' not found in body" });
+    if (!req.body.email) return res.status(404).json({ erro: true, message: "'email' not found in body" });
+    if (!req.body.telefone) return res.status(404).json({ erro: true, message: "'telefone' not found in body" });
+    if (!req.body.cargo) return res.status(404).json({ erro: true, message: "'cargo' not found in body" });
+    if (!req.body.tamanhoEmpresa) return res.status(404).json({ erro: true, message: "'tamanhoEmpresa' not found in body" });
+    if (!req.body.setor) return res.status(404).json({ erro: true, message: "'setor' not found in body" });
+    if (!req.body.necessidade) return res.status(404).json({ erro: true, message: "'necessidade' not found in body" });
+    if (!req.body.origem) return res.status(404).json({ erro: true, message: "'origem' not found in body" });
+>>>>>>> debfc91 (hotfix: remove field 'cargo: 3358f544d27546aebcfeed9a4d4ada87fccbbe29')
     next();
 }
 
--- src/services/pipedrive.service.js
+++ src/services/pipedrive.service.js
@@ -29,7 +29,19 @@ module.exports = class PipedriveService {
             const api = new pipedrive.DealsApi();
             const data = {
                 title: params.titulo,
+<<<<<<< HEAD
                 pipeline_id: params.pipelineId
+=======
+                pipeline_id: params.pipelineId,
+                "1c789bc9694dd2d61f0c4c8b073ee433365dca97": `${params.nome} ${params.sobrenome}`,
+                "1019bb808f1594729c22368b5436ca7a17b0df87": params.email,
+                "58ef1435017cc0c4aca73613a13afa499a611cad": params.telefone,
+                "3358f544d27546aebcfeed9a4d4ada87fccbbe29": params.cargo,
+                "1ce7fa13bc199dee8c04a3138756b0f8bba7b1c0": `${params.titulo}, setor: ${params.setor}`,
+                "80b23c7b4cc6a8f7cb2152863011d45006fd497d": params.tamanhoEmpresa,
+                "e07b41faef221c00f7401d4ee437a1d29b3597dd": params.necessidade,
+                "8ed844b1472908191334766dfd2c1b54fc9f616e": params.origem
+>>>>>>> debfc91 (hotfix: remove field 'cargo: 3358f544d27546aebcfeed9a4d4ada87fccbbe29')
             }
             const response = await api.addDeal(data);
 
@@ -40,4 +52,8 @@ module.exports = class PipedriveService {
         }
         return this.retorno;
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> debfc91 (hotfix: remove field 'cargo: 3358f544d27546aebcfeed9a4d4ada87fccbbe29')
